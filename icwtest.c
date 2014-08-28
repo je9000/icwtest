2b159ef749f66257953ff31fb40f9b93eea4a795
John Eaglesham <johne@apple.com>
2014-08-28 10:14:21 -0700

    Update Copyright to reflect code written by both entities.

--- icwtest.c
+++ icwtest.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2013, Yahoo! Inc All rights reserved.
+ * Copyright (c) 2013-2014, John Eaglesham All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
